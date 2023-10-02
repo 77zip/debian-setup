7413ef3af31bbe6747b4ada362c80871a782397a
_7zip <80418545+77zip@users.noreply.github.com>
2023-10-02 18:34:55 -0400

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,3 +1,3 @@
-sed 's/main non-free-firmware/main non-free-firmware contrib non-free/g' /etc/apt/sources.list > /etc/apt/sources.list
+sed 's/main non-free-firmware/main non-free-firmware contrib non-free/g' /etc/apt/sources.list
 apt-get update
 apt-get install nvidia-driver
